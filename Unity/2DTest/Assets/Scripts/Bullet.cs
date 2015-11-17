99a8b58c953ef1f77436c4cb41fa3ffb763c4071
akinobufujii <aki.develop@gmail.com>
2015-11-17 15:26:26 +0900

    「第05回 当たり判定とアニメーションイベントとレイヤー」の内容をコミット

--- Unity/2DTest/Assets/Scripts/Bullet.cs
+++ Unity/2DTest/Assets/Scripts/Bullet.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour
 {
+	// 弾の移動スピード
 	public int speed = 10;
 
-	// Use this for initialization
+	// ゲームオブジェクト生成から削除するまでの時間
+	public float lifeTime = 5;
+
 	void Start()
 	{
-
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
+		// ローカル座標のY軸方向に移動する
 		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+
+		// lifeTime秒後に削除
+		Destroy(gameObject, lifeTime);
 	}
 }
---
+++ Unity/2DTest/Assets/Scripts/Explosion.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+	// アニメーション終了後
+	void OnAnimationFinish()
+	{
+		Destroy(gameObject);
+	}
+}
